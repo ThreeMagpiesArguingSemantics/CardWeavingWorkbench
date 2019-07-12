ada96853cd19ddde265ecc506615b6b45c59dada
Twoffles <august.baaz@gmail.com>
2019-07-12 11:26:37 +0200

    Generalized styling
    And a bunch of other stuff that i really should have made into their own commits but meh

--- source/main.js
+++ source/main.js
@@ -17,7 +17,7 @@ var CardWeaver = (function(){
 	Ribbon.prototype.setWidth = function(width){
 		if (this.cards.length<width){
 			for(var k=width-this.cards.length; k>0; k--){
-				this.addCard();
+				var card = this.newCard();
 			}
 		}
 		else if (this.cards.length>width){
@@ -46,15 +46,25 @@ var CardWeaver = (function(){
 	}
 	Ribbon.prototype.newCard = function(index){
 		var card = new Card();
+		this.width++;
 		if (!index){
+			index = this.width-1;
 			this.cards.push(card);
-			this.twists.push(new Array());
+			this.twists.push(new Array(this.length));
 		}
 		else{
 			this.cards.splice(index, 0 , card);
 			this.twists.splice(index, 0 , new Array());
 		}
-		this.width++;
+
+		for(var k=0;k<this.width;k++){
+				card.setString(k,0);
+		}
+		for(var k=0;k<this.length;k++){
+			ribbon.setTwist(k,index,1);
+		}
+		card.setThreading("s");
+
 		return card;
 	}
 	Ribbon.prototype.removeCard = function(index){
@@ -66,15 +76,10 @@ var CardWeaver = (function(){
 		return card;
 	}
 	Ribbon.prototype.setTwist = function(row, column, value){
-		if (this.getColumnPeriod(column)-1 < row) row = getColumnPeriod(column);
 		this.twists[column][row] = value;
 	}
 	Ribbon.prototype.swapTwist = function(row, column){
-		if (this.getColumnPeriod(column)-1 < row) row = getColumnPeriod(column);
 		this.twists[column][row]*=-1;
-	}
-	Ribbon.prototype.getColumnPeriod = function(column){
-		return this.twists[column].length;
 	}
 	Ribbon.prototype.getCard = function(column){
 		return this.cards[column];
@@ -84,7 +89,7 @@ var CardWeaver = (function(){
 		var value;
 		for(column=0; column<this.width; column++){
 			if (this.cards[column].isValid()){
-				value = this.twists[column][loopValue(row,this.twists[column].length-1)];
+				value = this.twists[column][loopValue(row,this.length-1)];
 				value*=-1;
 			}
 			else{
@@ -101,7 +106,7 @@ var CardWeaver = (function(){
 			if (this.cards[column].isValid()){
 				value = 0;
 				for(k2=0;k2<row; k2++){
-					value+=this.twists[column][loopValue(k2,this.twists[column].length-1)];
+					value+=this.twists[column][loopValue(k2,this.length-1)];
 				}
 				value = loopValue(value,this.cards[column].holes,1);
 			}
@@ -143,6 +148,7 @@ var CardWeaver = (function(){
 		for(var string of data.strings){
 			var s = this.newString();
 			s.color = string.color;
+			s.name = string.name;
 		}
 
 		for(var card of data.cards){
@@ -184,8 +190,8 @@ var CardWeaver = (function(){
 	}
 	Card.prototype.setStrings = function(){
 		this.strings = [];
-		for(var k=0; k<this.holes && k<arguments[k].length; k++){
-			this.setString(index, arguments[k])
+		for(var k=0; k<this.holes && k<arguments.length; k++){
+			this.setString(k, arguments[k])
 		}
 	}
 	Card.prototype.setThreading = function(threading){
@@ -212,9 +218,13 @@ var CardWeaver = (function(){
 
 	var String = function(){
 		this.color;
+		this.name;
 	}
 	String.prototype.setColor = function(hex){
 		this.color = hex;
+	}
+	String.prototype.setName = function(string){
+		this.name = string;
 	}
 
 	var loopValue = function(value, max, min=0){
@@ -230,14 +240,32 @@ var CardWeaver = (function(){
 
 var ribbon = new CardWeaver.Ribbon();
 
-function setup(){
-	ribbon.load('{"width":8,"length":8,"cards":[{"holes":4,"strings":[2,2,2,2],"threading":"z"},{"holes":4,"strings":[1,0,1,0],"threading":"z"},{"holes":4,"strings":[0,1,0,1],"threading":"z"},{"holes":4,"strings":[1,0,1,0],"threading":"z"},{"holes":4,"strings":[0,1,0,1],"threading":"s"},{"holes":4,"strings":[1,0,1,0],"threading":"s"},{"holes":4,"strings":[0,1,0,1],"threading":"s"},{"holes":4,"strings":[2,2,2,2],"threading":"s"}],"twists":[[-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,1,1,1,1],[-1,-1,-1,-1,1,1,1,1],[-1,-1,-1,-1,1,1,1,1],[1,1,1,1,-1,-1,-1,-1],[1,1,1,1,-1,-1,-1,-1],[1,1,1,1,-1,-1,-1,-1],[1,1,1,1,1,1,1,1]],"strings":[{"color":"blue"},{"color":"red"},{"color":"grey"}]}')
-	drawRibbon(40);
+function swapTwist(e){
+	e = e || window.event;
+	var element = e.target || e.srcElement;
+	var cell;
+	var row;
+	if (element.nodeName.toLowerCase() == "td"){
+		column = element.cellIndex;
+		row = element.parentNode.rowIndex;
+		row = document.getElementById("ribbonPreview").getElementsByTagName("tr").length-1 - row;
+	}
+	else return;
+
+	ribbon.swapTwist(row, column);
 	updateRibbon();
-	drawCardSettings()
+	//alert('You click on row ' + row+" and cell "+column);
 }
 
-function drawRibbon(length){
+function setup(){
+	ribbon.load('{"width":8,"length":8,"cards":[{"holes":4,"strings":[2,2,2,2],"threading":"z"},{"holes":4,"strings":[1,0,1,0],"threading":"z"},{"holes":4,"strings":[0,1,0,1],"threading":"z"},{"holes":4,"strings":[1,0,1,0],"threading":"z"},{"holes":4,"strings":[0,1,0,1],"threading":"s"},{"holes":4,"strings":[1,0,1,0],"threading":"s"},{"holes":4,"strings":[0,1,0,1],"threading":"s"},{"holes":4,"strings":[2,2,2,2],"threading":"s"}],"twists":[[-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,1,1,1,1],[-1,-1,-1,-1,1,1,1,1],[-1,-1,-1,-1,1,1,1,1],[1,1,1,1,-1,-1,-1,-1],[1,1,1,1,-1,-1,-1,-1],[1,1,1,1,-1,-1,-1,-1],[1,1,1,1,1,1,1,1]],"strings":[{"color":"blue","name":"blue_silk"},{"color":"red","name":"red_silk"},{"color":"grey","name":"silver_silk"}]}')
+	drawRibbon();
+	drawcardSettings()
+	drawFormatSettings()
+}
+
+function drawRibbon(){
+	var length = ribbon.length*3
 	var ribbonPreview = document.getElementById("ribbonPreview");
 	var turnInstructions = document.getElementById("turnInstructions");
 	ribbonPreview.innerHTML = "";
@@ -279,115 +307,167 @@ function updateRibbon(){
 			}
 		}
 	}
+
+	var parrent = document.getElementById("pattern");
+	parrent.scrollTop = parrent.scrollHeight //NOTE
 }
 
-function drawCardSettings(){
-	var parrent = document.getElementById("card_settings");
+function drawcardSettings(){
+	var parrent = document.getElementById("cardSettings");
 	parrent.innerHTML = "";
 
-	// tabs
-	var tabs = document.createElement("ul");
-	tabs.onclick = function(e){
-		e = e || window.event;
-		var element = e.target || e.srcElement;
-		if (element.nodeName.toLowerCase() != "li")return;
-
-		var sections = document.getElementById("card_settings").getElementsByClassName("section");
-		for(var sec of sections){
-			sec.className = "section";
-		}
-
-		var tabs = document.getElementById("card_settings").getElementsByTagName("li");
-		for(var tab of tabs){
-			tab.style = "";
-		}
-		element.style.backgroundColor="white";
-		sections[element.value].className = "section active";
+	var tabs = [];
+	for(var k=0;k<ribbon.width;k++){
+		tabs.push(""+k)
 	}
-	for(var k=0; k<ribbon.width; k++){
-		var tab = document.createElement("li");
-		var text = document.createTextNode(k+1+"");
-		tab.appendChild(text);
-		tab.value=k;
-		tabs.appendChild(tab);
-	}
-	parrent.appendChild(tabs);
-	document.getElementById("card_settings").getElementsByTagName("li")[0].style.backgroundColor="white";
 
-	// sections
-	for(var k=0; k<ribbon.width; k++){
-		let card = ribbon.cards[k];
+	drawSectionTabs(parrent, tabs, 0);
 
-		// holes
-		let section = document.createElement("div");
-		section.className="section";
-
-		parrent.appendChild(section);
-	}
-	document.getElementById("card_settings").getElementsByClassName("section")[0].className="section active";
-
-	updateCardSettings();
+	updatecardSettings();
 }
-function updateCardSettings(){
-	var sections = document.getElementById("card_settings").getElementsByClassName("section");
-
+function updatecardSettings(){
+	var parrent = document.getElementById("cardSettings");
+	var sections = parrent.getElementsByClassName("section");
 
 	for(var k=0; k<ribbon.width; k++){
 		let section = sections[k];
 		section.innerHTML="";
 
 		let card = ribbon.cards[k];
-		let holes_selector = document.createElement("input");
-		holes_selector.type = "number";
-		holes_selector.max = 12;
-		holes_selector.min = 2;
-		holes_selector.value = card.holes;
+		let holes_selector = drawNumberInput(section,"card_holes", 2, card.holes)
 		holes_selector.onchange = function(){
-			card.setHoles(holes_selector.valueAsNumber);
-			updateCardSettings();
+			card.setHoles(holes_selector.valueAsNumber); // NOTE needs to validate
+			updatecardSettings();
 			updateRibbon();
 		};
-		section.appendChild(holes_selector);
 
 		// strings
+		var options = [];
+		for(string of ribbon.strings){
+			options.push(string.name);
+		}
+
 		for(var k2=0; k2<card.holes; k2++){
-
-			let string_selector = document.createElement("select");
-			string_selector.style.backgroundColor = ribbon.strings[card.strings[k2]].color;
-			let index = k2;
-			string_selector.onchange=function(){
-				string_selector.style.backgroundColor = ribbon.strings[string_selector.value].color;
-				card.setString(index,string_selector.value);
-				updateRibbon();
-			}
-
-			for(var k3=0; k3<ribbon.strings.length; k3++){
-				var option = document.createElement("option");
-				if (card.strings[k2] == k3) option.selected = "selected";
-				option.value = k3;
-				option.innerHTML = "string "+k3;
-				option.style.backgroundColor = ribbon.strings[k3].color;
-				string_selector.appendChild(option);
-			}
-
-			section.appendChild(string_selector);
+				let strings = drawSelectInput(section,String.fromCharCode(65+k2),options,card.strings[k2])
+				strings.style.backgroundColor = ribbon.strings[card.strings[k2]].color;
+				let index = k2;
+				strings.onchange=function(){
+					strings.style.backgroundColor = ribbon.strings[strings.selectedIndex].color;
+					card.setString(index,strings.selectedIndex);
+					updateRibbon();
+				}
 		}
 	}
 }
 
-function swapTwist(e){
-	e = e || window.event;
-	var element = e.target || e.srcElement;
-	var cell;
-	var row;
-	if (element.nodeName.toLowerCase() == "td"){
-		column = element.cellIndex;
-		row = element.parentNode.rowIndex;
-		row = document.getElementById("ribbonPreview").getElementsByTagName("tr").length-1 - row;
-	}
-	else return;
+function drawFormatSettings(){
+		var parrent = document.getElementById("formatSettings");
+		let width = drawNumberInput(parrent,"ribbon_width", 4, ribbon.width)
+		width.onchange = function(){
+			ribbon.setWidth(width.valueAsNumber);
+			drawcardSettings();
+			drawRibbon();
+		};
 
-	ribbon.swapTwist(row, column);
-	updateRibbon();
-	//alert('You click on row ' + row+" and cell "+column);
+
+		let length = drawNumberInput(parrent,"ribbon_length", 4, ribbon.length)
+		length.onchange = function(){
+			ribbon.setLength(length.valueAsNumber);
+			drawcardSettings();
+			drawRibbon();
+		};
+}
+
+function drawSectionTabs(parrent, tabNames, selected){
+
+	// tabs
+	var tabs = document.createElement("ul");
+	tabs.onclick = function(e){
+		// validate click
+		e = e || window.event;
+		var element = e.target || e.srcElement;
+		if (element.nodeName.toLowerCase() != "li")return;
+
+		var parrent = element.parentNode.parentNode;
+		var sections = parrent.getElementsByClassName("section");
+
+		// reset all
+		for(var sec of sections){
+			sec.className = "section";
+		}
+		var tabs = parrent.getElementsByTagName("li");
+		for(var k=0;k<tabs.length;k++){
+			tabs[k].className = "";
+
+			if (tabs[k].value == element.value){
+				element.className="active";
+				sections[k].className = "section active";
+			}
+		}
+		// set new
+
+	}
+
+	for(var k=0; k<tabNames.length; k++){
+		var tab = document.createElement("li");
+		var text = document.createTextNode(tabNames[k]);
+		tab.appendChild(text);
+		tab.value=k;
+		if (k==selected){
+			tab.className="active";
+		}
+		tabs.appendChild(tab);
+	}
+	parrent.appendChild(tabs);
+
+	// sections
+	sections = [];
+	for(var k=0; k<tabNames.length; k++){
+		// holes
+		let section = document.createElement("div");
+		section.className="section";
+		sections.push(section);
+		parrent.appendChild(section);
+	}
+	sections[selected].className="section active";
+
+	return sections;
+}
+
+function drawSelectInput(parrent, name,options,selected){
+	var w = document.createElement("div")
+	w.className="inputWrap"
+	var l = document.createElement("label");
+	l.htmlFor=name;
+	l.innerHTML=name+":";
+	w.appendChild(l)
+	var s = document.createElement("select");
+	for(var k=0; k<options.length; k++){
+		var o = document.createElement("option");
+		if (selected == k) o.selected = "selected";
+		o.style.backgroundColor="white";
+		o.value = k;
+		o.innerHTML = options[k];
+		s.appendChild(o);
+	}
+	w.appendChild(s)
+	parrent.appendChild(w);
+	return s;
+}
+
+function drawNumberInput(parrent, name,minValue,startValue){
+	var w = document.createElement("div")
+	w.className="inputWrap"
+	var l = document.createElement("label");
+	l.htmlFor=name;
+	l.innerHTML=name+":";
+	w.appendChild(l)
+	var e = document.createElement("input");
+	e.type = "number";
+	e.min = minValue;
+	e.id=name;
+	e.value = startValue;
+	w.appendChild(e);
+	parrent.appendChild(w);
+	return e
 }
